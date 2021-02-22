a3d83fdf02bf5b155602e6e2cadd61533f384748
Your Name <you@example.com>
2021-02-22 11:13:21 +0530

    adding all file of library management

---
+++ javacript/ap.js
@@ -0,0 +1,8 @@
+$(document).ready(()=>{
+    $("#fadeCard").click(()=>{
+        $(".bcardjs1").fadeIn();
+        $(".bcardjs2").fadeIn("slow");
+        $(".bcardjs3").fadeIn(3000);
+        $(".bcardjs4").fadeIn(5000);
+    })
+})
